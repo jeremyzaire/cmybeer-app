5333d3b1661e0eeae157d7cfb386a9c7a1408fcc
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-03-10 17:10:45 +0100

    feat: add routing to the application

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,16 @@
-import CountDown from "../CountDown";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "../HomeScreen";
+import BasketScreen from "../BasketScreen";
+import BeerScreen from "../BeerScreen";
 
 export default function App() {
   return (
-    <>
-      <CountDown initialCount={12} />;
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/basket" element={<BasketScreen />} />
+        <Route path="/beers/:id" element={<BeerScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
