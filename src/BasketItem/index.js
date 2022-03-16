b6bcd95848e0fcb51241152d2b044c6f20594b18
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-03-16 15:09:57 +0100

    feat: prepare basket items on price

---
+++ src/BasketItem/index.js
@@ -0,0 +1,44 @@
+import { Image } from "@mui/icons-material";
+import { string, number } from "prop-types";
+import {
+  Avatar,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import BasketItemPrice, { types as basketItemPrice } from "../BasketItemPrice";
+
+export default function BasketItem({
+  name,
+  imageUri,
+  price,
+  quantity,
+  divider = true,
+}) {
+  const basketItemPrice = () => (
+    <>
+      <strong>{price}</strong> x{quantity}
+    </>
+  );
+
+  return (
+    <>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar src={imageUri} alt={name}>
+            <Image />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={name} secondary={basketItemPrice} />
+      </ListItem>
+      {divider && <Divider variant="inset" component="li" />}
+    </>
+  );
+}
+
+BasketItem.propTypes = {
+  name: string,
+  divider: Boolean,
+  ...basketItemPriceTypes,
+};
