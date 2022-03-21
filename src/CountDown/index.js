6020fe30cc702f36d4a775a5be51befbf5a1b507
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-03-21 14:45:49 +0100

    fix: fix test

--- src/CountDown/index.js
+++ src/CountDown/index.js
@@ -1,27 +1,26 @@
-import "./style.css";
 import { number } from "prop-types";
 import React from "react";
+
+import "./styles.css";
 import { useCounter } from "../hooks";
 
-export default function CountDown({ initialCount = 100, step = 1 }) {
+export default function Countdown({ initialCount = 100, step = 1 }) {
   const [count, { decrement, reset }] = useCounter(initialCount, step);
 
   return (
     <>
-      <p role="timer" aria-live="polite" className="countdown-number">
+      <p role="timer" aria-label="counter" className="countdown-number">
         {count}
       </p>
       <button className="countdown-button" onClick={decrement}>
-        CountDown
+        Countdown
       </button>
-      <button className="reset-button" onClick={reset}>
-        Reset
-      </button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 }
 
-CountDown.propTypes = {
+Countdown.propTypes = {
   initialCount: number,
   step: number,
 };
