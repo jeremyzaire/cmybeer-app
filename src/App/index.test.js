b5c43323f2627a8f39c0c1692b742f831e9c44b6
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-03-21 10:45:25 +0100

    test: add test for app

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -3,6 +3,6 @@ import App from ".";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByRole("heading", { level: 1, name: "CMyBeer" });
+  expect(title).toBeInTheDocument();
 });
