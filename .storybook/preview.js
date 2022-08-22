ce60f8d9f0125ede4379d440b06122d39cf9586e
CDBDX\jeremy.zaire <jeremy.zaire@cdiscount.com>
2022-08-22 09:04:45 +0200

    refactor: update changes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { QueryClientProvider } from "react-query";
+
+import theme from "../src/ds/theme";
+import { BasketContext } from "../src/contexts";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +12,24 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 };
+
+const queryClient = new QueryClient();
+
+export const decorators = [
+  (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <BasketContext.Provider>
+        <ThemeProvider theme={theme}>
+          <Story />
+        </ThemeProvider>
+      </BasketContext.Provider>
+    </QueryClientProvider>
+  ),
+];
